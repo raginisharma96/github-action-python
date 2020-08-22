99f3b0c86709e80b82866bd09e5dd0ff314e945a
Ragini Sharma <raginisharma1709@gmail.com>
2020-08-22 16:49:20 +0530

    updated test.py

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from src.welcome import greet
 
 def test_greet():
     name = "Emma"
-    output = "Welcome to the year 2020, Emma"
+    output = "Welcome to the year 2020 Emma"
     assert(greet(name) == output)
