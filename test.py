2f0add765f082fbec7077cdfefbc774003feb981
Ragini Sharma <raginisharma1709@gmail.com>
2020-08-24 15:34:10 +0530

    added test case for validateAge

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
 from src.welcome import greet
+from src.ageLimit import validateAge
 
 def test_greet():
     name = "Emma"
     output = "Welcome to the year 2020, Emma"
     assert(greet(name) == output)
+
+def test_validateAge():
+    age1 = 20
+    age2 = 14
+    output1 = True
+    output2 = False
+    assert(validateAge(age1) == output1)
+    assert(validateAge(age2) == output2)
